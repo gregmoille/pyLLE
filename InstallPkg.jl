7ab542961421b6f170f9d0e33b61bfdeb204504b
gregmoille <greg.moille@gmail.com>
2018-08-08 14:03:03 -0400

    Make it Windows Compatible

--- InstallPkg.jl
+++ InstallPkg.jl
@@ -1,6 +1,23 @@
-print("\nAdding HDF5 package to julia\n")
-Pkg.add("HDF5")
-Pkg.update("HDF5")
-print("\nAdding ProgressMeter package to julia\n")
-Pkg.add("ProgressMeter")
-Pkg.update("ProgressMeter")
+
+isinstalled(pkg::AbstractString) = pkg != "METADATA" && pkg != "REQUIRE" && pkg[1] != '.' && Pkg.cd(isdir, pkg)
+
+print("\nHDF5\n")
+if isinstalled("HDF5")
+    print("HDF5 already installed\n")
+else
+    print("\nAdding HDF5 package to julia\n")
+    Pkg.add("HDF5")
+    Pkg.update("HDF5")
+end
+
+
+print("\ProgressMeter\n")
+if isinstalled("ProgressMeter")
+    print("ProgressMeter already installed\n")
+else
+    print("\nAdding ProgressMeter package to julia\n")
+    Pkg.add("ProgressMeter")
+    Pkg.update("ProgressMeter")
+end
+
+
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(name='pyLLE',
-      version='1.1.7',
+      version='1.1.9',
       description='LLE Solver',
       url='https://github.com/gregmoille/pyLLE',
       author='Greg Moille',
