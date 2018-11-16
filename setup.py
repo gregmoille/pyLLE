1bb7ecf6ba4a063cc53aa5bac3e268363958e76f
gregmoille <greg.moille@gmail.com>
2018-11-15 19:37:03 -0500

    Update Readme

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(name='pyLLE',
-      version='2.0',
+      version='2.1',
       description='LLE Solver',
       url='https://github.com/gregmoille/pyLLE',
       author='Greg Moille',
