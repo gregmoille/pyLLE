a0d4d5edb2e46fac9e232cdd7d2d0562b621a852
gregmoille <gmoille@umd.edu>
2019-07-08 21:15:53 -0400

    Update install

--- setup.py
+++ setup.py
@@ -15,17 +15,17 @@ class MyInstall(install):
         if sys.platform == 'linux2':
             julia = 'julia'
         if sys.platform == 'win32':
-            julia = os.path.expanduser('~') + '\\AppData\\Local\\Julia-0.6.4\\bin\\julia.exe'
+            julia = os.path.expanduser('~') + '\\AppData\\Local\\Julia-1.1.4\\bin\\julia.exe'
         sub.call([julia, 'InstallPkg.jl'])
 
 
 setup(name='pyLLE',
-      version='2.1.2',
+      version='3.0.0',
       description='LLE Solver',
       url='https://github.com/gregmoille/pyLLE',
       author='Greg Moille',
       author_email='gregory.moille@nist.gov',
-      license='MIT',
+      license='Open',
       long_description='',
       packages=['pyLLE'],
       install_requires=[
@@ -43,6 +43,6 @@ setup(name='pyLLE',
       cmdclass={'install': MyInstall},
       classifiers=(
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved",
         "Operating System :: OS Independent",
     ),)
